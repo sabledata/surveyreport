ce71ba231bab3c8977bb860f372682eb7ba690a2
sabledata <55628556+sabledata@users.noreply.github.com>
2020-06-23 12:27:09 -0700

    updated figure 13 with A and B

--- standaloneData/getData.R
+++ standaloneData/getData.R
@@ -349,7 +349,7 @@ history     <- paste("select dbo.SURVEY_SITE_HISTORIC.SURVEY_SERIES_ID, YEAR(dbo
     write.table( polar , file = paste(path,"figure12_Polar.csv",sep=''),row.names=FALSE, na="",col.names=TRUE, sep=",")
     write.table(polarsummary, file = paste(path,"figure12_PolarSummary.csv",sep=''),row.names=FALSE, na="",col.names=TRUE, sep=",")
 
-   dt   <-  paste("select * from Report_Survey_GFBIO_Age_MF_Prop where SetType='StRS' and Year<=", yr + 1, sep="")
+   dt   <-  paste("select * from Report_Survey_GFBIO_Age_MF_Prop where SetType='StRS' and Year<=", yr, sep="")
    dat  <-  GetSQLData(dt,"Sablefish")
    write.table(dat, file = paste(path,"figure13_AgeBubblePlot.csv",sep=''),row.names=FALSE, na="",col.names=TRUE, sep=",")
 
